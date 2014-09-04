1330a50ee233d8d31ef3f25b5ba570ade3c5ac21
youribonnaffe <youri.bonnaffe@activeeon.com>
2014-09-04 17:51:43 +0200

    DOC-227 Add remote visu documentation

---
+++ src/docs/user/examples/remote_visualization_script.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# Create a X server with Xvnc and run a simple graphical app
+
+Xvnc :10 -geometry 1280x1024 &
+xvnc_pid=$!
+ps -p $xvnc_pid
+if [ $? -eq 0 ]; then
+    # magic string to enable remote visualization
+    echo "PA_REMOTE_CONNECTION;$PAS_TASK_ID;vnc;$(hostname):$((5910))"
+    export DISPLAY=:10
+    # RUN YOUR GUI BASED APPLICATION HERE
+    xclock
+
+    kill $xvnc_pid
+        echo "[debug] Display closed"
+        exit
+fi
