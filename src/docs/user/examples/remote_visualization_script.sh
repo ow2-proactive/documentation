b72c82b38cf59204dc1d3d24f465e86751b0a253
mbenguig <michael.benguigui@activeeon.com>
2015-12-08 10:01:58 +0100

    Update the magic string in remote_visualization_script.sh (including both the job ID and task ID)

--- src/docs/user/examples/remote_visualization_script.sh
+++ src/docs/user/examples/remote_visualization_script.sh
@@ -6,7 +6,7 @@ xvnc_pid=$!
 ps -p $xvnc_pid
 if [ $? -eq 0 ]; then
     # magic string to enable remote visualization
-    echo "PA_REMOTE_CONNECTION;$variables_PA_TASK_ID;vnc;$(hostname):$((5910))"
+    echo "PA_REMOTE_CONNECTION;$variables_PA_JOB_ID;$variables_PA_TASK_ID;vnc;$(hostname):5910"
     export DISPLAY=:10
     # RUN YOUR GUI BASED APPLICATION HERE
     xclock
